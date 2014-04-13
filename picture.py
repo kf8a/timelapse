810356e7137e0174233f150379acb1b6af3f7a68
sven bohm <bohms@msu.edu>
2014-04-13 16:17:44 -0400

    removed log statements

--- picture.py
+++ picture.py
@@ -15,12 +15,7 @@ today = ephem.localtime(ephem.now()).date()
 rising = ephem.localtime(observer.previous_rising(sun)).date()
 setting = ephem.localtime(observer.next_setting(sun)).date()
 
-print today
-print  rising
-print setting
-
 if (today == rising) & (today == setting):
-  print "daylight"
   capture = cv.CaptureFromCAM(0)
   cv.SetCaptureProperty(capture,CV_CAP_PROP_FRAME_HEIGHT,960)
   cv.SetCaptureProperty(capture,CV_CAP_PROP_FRAME_WIDTH,1280)
