4c7d1c8b102b983745ff21c6d25b35ea8e7ce69a
sven bohm <bohms@msu.edu>
2014-04-19 20:33:13 -0400

    take a couple of pictures to get the exposure right

--- picture.py
+++ picture.py
@@ -1,9 +1,14 @@
 import cv
+import os
+import time
 import ephem
 from datetime import date, datetime
 
-CV_CAP_PROP_FRAME_WIDTH = 3
-CV_CAP_PROP_FRAME_HEIGHT = 4
+CV_CAP_PROP_POS_FRAMES    = 1
+CV_CAP_PROP_FRAME_WIDTH   = 3
+CV_CAP_PROP_FRAME_HEIGHT  = 4
+CV_CAP_PROP_BRIGHTNESS    = 10
+CV_CAP_PROP_EXPOSURE      = 15
 
 observer = ephem.Observer()
 observer.lon = "-85:24"
@@ -17,13 +22,17 @@ setting = ephem.localtime(observer.next_setting(sun)).date()
 
 if (today == rising) & (today == setting):
   capture = cv.CaptureFromCAM(0)
-  cv.SetCaptureProperty(capture,CV_CAP_PROP_FRAME_HEIGHT,960)
+  cv.SetCaptureProperty(capture,CV_CAP_PROP_FRAME_HEIGHT,720)
   cv.SetCaptureProperty(capture,CV_CAP_PROP_FRAME_WIDTH,1280)
 
+  frame = cv.QueryFrame(capture)
+  time.sleep(1)
+  frame = cv.QueryFrame(capture)
+  time.sleep(1)
   frame = cv.QueryFrame(capture)
   filename = '/home/pi/images/'
   filename += str(datetime.now().isoformat())
   filename += '.png'
   cv.SaveImage(filename,frame)
-
-
+  os.unlink("/home/pi/images/current.png")
+  os.symlink(filename, "/home/pi/images/current.png")
